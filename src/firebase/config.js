f5edc4c7229ea4b57b8c330a7f59ce60915d49b3
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-05-26 14:46:55 +0400

    firebase config file added

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,1 +1,16 @@
 import firebase from 'firebase'
+
+
+//https://console.firebase.google.com/project/olx-demo-e1568/settings/general/web:MTc2NTA3YzItZTA3Yi00M2M3LTg2NjMtNDNiYTUyNTdhYjY5
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// this code is copied from firebase >project overview >config 
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBGG9BL4osat3wRSbcdkR_AkVBR6PzVrgE",
+    authDomain: "olx-demo-e1568.firebaseapp.com",
+    projectId: "olx-demo-e1568",
+    storageBucket: "olx-demo-e1568.appspot.com",
+    messagingSenderId: "836762922355",
+    appId: "1:836762922355:web:cda19b59a24527d9326906",
+    measurementId: "G-1Y80YRHFS3"
+  };
